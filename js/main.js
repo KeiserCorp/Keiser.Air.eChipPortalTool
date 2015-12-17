f8f5a26551b2930d6da0f7e55745f10a67e82997
Zach Cardoza <bayssmekanique@live.com>
2015-12-17 13:17:29 -0800

    Implements basic html structure and styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict';
-var ow = (require('1-wire-js'))();
-var Q = require('q');
+var ow = require('1-wire-js');
 
-console.log(ow);
+var webPortal = document.getElementById('webPortal');
+
+window.onload = function () {
+	webPortal.src = 'https://ips.keiser.com/';
+}
