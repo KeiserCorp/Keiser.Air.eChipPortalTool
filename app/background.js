1d89824708c34aeb7df8488909fd011c10bd5fdf
Zach Cardoza <bayssmekanique@live.com>
2016-01-04 13:03:59 -0800

    Reformats background.js file

--- app/background.js
+++ app/background.js
@@ -4,12 +4,10 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create(
-    'index.html',
-    {
-      id: 'mainWindow',
-	  state: 'maximized'
-    }
-  );
+chrome.app.runtime.onLaunched.addListener(function (launchData) {
+	chrome.app.window.create(
+		'index.html', {
+		id : 'mainWindow',
+		state : 'maximized'
+	});
 });
