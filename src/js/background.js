264b0ccd8dc82b184eda170d31e4b8aa5ce1e9a4
Zach Cardoza <bayssmekanique@live.com>
2016-03-30 10:13:27 -0700

    Reformats style

--- src/js/background.js
+++ src/js/background.js
@@ -1,14 +1,23 @@
 var ow = require('1-wire-js');
-chrome.app.runtime.onLaunched.addListener(function (launchData) {
-	chrome.app.window.create(
-		'index.html', {
-			id: 'eChipPortalTool',
-			state: 'maximized'
-		},
-		function (appWindow) {
-			appWindow.contentWindow.ow = ow;
-			appWindow.onClosed.addListener(function () {
-				ow.device.close();
+chrome
+	.app
+	.runtime
+	.onLaunched
+	.addListener(function(launchData) {
+		chrome
+			.app
+			.window
+			.create('index.html', {
+				id: 'eChipPortalTool',
+				state: 'maximized',
+			}, function(appWindow) {
+				appWindow.contentWindow.ow = ow;
+				appWindow
+					.onClosed
+					.addListener(function() {
+						ow
+							.device
+							.close();
+					});
 			});
-		});
-});
+	});
